7e50f4ed04d7efcbd80372e493d813c52235ed40
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-03-08 21:51:04 -0300

    modificando o index.js com o dayjs

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var moment = require('moment');
+var dayjs = require('dayjs');
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
+
+console.log(dayjs().format())

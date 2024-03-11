eed04a9e7200a879d9538f782289eb7dab02cb82
Diogo S. Florêncio <33941005+diogosflorencio@users.noreply.github.com>
2024-03-11 20:52:13 -0300

    codigo js 2015

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var moment = require('moment');
-var dayjs = require('dayjs');
+import moment from 'moment';
+import dayjs from 'dayjs';
 
 console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
 
 console.log(dayjs().format())
+
+var name = "Bob", time = "today";  
+console.log(`Hello ${name}, how are you ${time}?`);
